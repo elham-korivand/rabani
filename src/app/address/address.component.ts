d73cc6f92bb3d262346b8bd6acd9c3694d961afc
korivand.e <korivand.e@rabani.com>
2022-05-26 14:14:24 +0330

    address2

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-address',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.css']
 })
 export class AddressComponent implements OnInit {
+  checked:boolean=false;
+  formregister = new FormGroup({
+    exampleInputEmail1: new FormControl(null, Validators.required),
+    exampleInputlastname: new FormControl(null, Validators.required),
+    exampleInputphone: new FormControl(null, Validators.required),
+    exampleInputstate: new FormControl(null, Validators.required),
+    exampleInputcity: new FormControl(null, Validators.required),
+    exampleFormaddress: new FormControl(null, Validators.required),
+    exampleInputcode: new FormControl(null, Validators.required),
+    exampleInputpelak: new FormControl(null, Validators.required),
+    exampleInputvahed: new FormControl(null, Validators.required),
+  });
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  openModalForm() {
+    console.log(this.formregister);
 
+  }
+  checkValue(PointerEvent:any){
+console.log(PointerEvent.target.checked);
+this.checked=PointerEvent.target.checked
+  }
+  alertAdress(){
+    if(!this.checked){
+      alert("ابتدا یک آدرس انتخاب کنید")
+    }
+  }
 }
